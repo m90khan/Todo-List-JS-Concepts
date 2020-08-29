e250be2fd97144c670ecff6261651689233061fc
Khan Mohsin <m90khan@gmail.com>
2020-08-29 21:02:43 +0500

    Update course

---
+++ js/softwareEng.js
@@ -0,0 +1,59 @@
+/*
+Topic: Software Engineering
+
+- SE is an process of combining tools, methods and standard operations to product quality softwares within given constraints.
+
+* Software Process: 
+- Breaking down the software complexity into systematic smaller pieces
+- using formal techniques on how will the software will be developed.
+
+* FOUR SOFTWARE PROCESSES
+- WATERFALL : one phase to the other , follows the flow
+- Evolutionary Prototyping: start with intial and develop based on customer feedback
+- Unified Software Process: heavily based on UML
+- AGILE: flexible in development, changes in requirements
+
+Topic: Software Phases
+- Requirements Engineering
+- Design (High level Structure)
+- Implementation
+- Verification and Validation
+- Maintanenece
+
+* Tools
+- IDE, VCS (GIT) , Coverage and Verification Tools
+
+* Software Lifecycle
+- Software Lifecycle : sequence of decisions that determine the history of your software
+- There are many models in which you can make decisions that better fulfill requirments. 
+
+* Requirments Engineering : iterative process
+- RE is the process of establishing the needs of stakeholders that are to be solved by software
+
+-1- Elicitation: Colelction of reuirments from stakeholders
+-2- Analysis: deeper understanding of requirments
+-3- Specification: REQ are organized so to be shared
+-4- Validation: validate reqs, that they are complete, consistent, non-redundant
+-5- Management: Accounts of changes to the requirments during the entire development process
+
+* Software Design
+- software requirements are analyses to describe the inner structure of the organization 
+- the description will server as a basis of teh actual system. has series of design phases.
+
+- Architectural Design -> Abstract Specification -> Interface Design
+-> Component Design -> Data Structure -> Algorithm Design
+-(we go from high level design system to a low-level)
+
+* Implementation
+- Realize the design system and proceed with implementation
+
+- Reduction of Complexity
+- Anticipation of Diversity: to anticipate that Software could change during ddevelopment
+- Structure Validation: easy testable software
+- agrees with internal or external standards
+
+
+* Validation and Verification
+- validation the system concurrent with requirments
+- verification: is the developed software is correctly build
+*/
